231ff7de3bf677dafb1225662bf247dba9f64532
mustafashokry555 <mustafashokry555@gmail.com>
2024-08-10 23:47:13 +0300

    un trans

--- resources/views/layout/mainlayout_admin.blade.php
+++ resources/views/layout/mainlayout_admin.blade.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
-  @if(session()->get('locale')=='ar')
-  <html dir="rtl" lang="ar">
-@else
-<html dir="ltr" lang="en">
-  @endif
-  <head>
-    @include('layout.partials.head_admin')
-  </head>
+<html>
 
-  <body>
+<head>
+    @include('layout.partials.head_admin')
+</head>
+
+{{-- 
+@if (session()->get('locale') == 'ar')
+  <body dir="rtl" lang="ar">
+@else 
+@endif
+--}}
+
+<body dir="ltr" lang="en">
     @include('layout.partials.header_admin')
     @include('layout.partials.nav_admin')
 
     @yield('content')
     @include('layout.partials.footer_admin-scripts')
-  </body>
-</html>
+</body>
+</body>
