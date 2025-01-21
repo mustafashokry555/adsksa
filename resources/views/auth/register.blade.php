057ba78a220bb6e70470295fd3998280b75944ce
Your Name <you@example.com>
2025-01-21 16:37:27 +0200

    registe as patient

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<?php $page = "index-13"; ?>
+<?php $page = 'index-13'; ?>
 @extends('layout.mainlayout_index1')
 @section('title', 'Register')
 @section('content')
@@ -24,8 +24,8 @@
                         <div class="account-content">
                             <div class="row align-items-center justify-content-center">
                                 <div class="col-md-7 col-lg-6 login-left">
-                                    <img src="{{ URL::asset('/assets/img/login-banner.png')}}" class="img-fluid"
-                                         alt="Doccure Register">
+                                    <img src="{{ URL::asset('/assets/img/login-banner.png') }}" class="img-fluid"
+                                        alt="Doccure Register">
                                 </div>
                                 <div class="col-md-12 col-lg-6 login-right">
                                     <div class="login-header">
@@ -36,48 +36,47 @@
                                     <form action="{{ route('register') }}" method="POST">
                                         @csrf
                                         <div class="form-group form-focus">
-                                            <input type="text" class="form-control floating" id="name" name="name"
-                                                   value="{{old('name')}}" required>
+                                            <input type="text" class="form-control floating" id="name_en"
+                                                name="name_en" value="{{ old('name_en') }}" required>
                                             <label class="focus-label">Name</label>
-                                            @error('name')
-                                            <div class="text-danger pt-2">
-                                                {{$message}}
-                                            </div>
+                                            @error('name_en')
+                                                <div class="text-danger pt-2">
+                                                    {{ $message }}
+                                                </div>
                                             @enderror
                                         </div>
                                         <div class="form-group form-focus">
                                             <input type="email" id="email_address" class="form-control floating"
-                                                   name="email" required>
+                                                name="email" required>
                                             <label class="focus-label"> Email</label>
                                             @error('email')
-                                            <div class="text-danger pt-2">
-                                                {{$message}}
-                                            </div>
+                                                <div class="text-danger pt-2">
+                                                    {{ $message }}
+                                                </div>
                                             @enderror
                                         </div>
                                         <div class="form-group form-focus">
-                                            <input type="password" id="password"
-                                                   class="form-control floating pass-input" name="password"
-                                                   value="{{old('password')}}" required>
+                                            <input type="password" id="password" class="form-control floating pass-input"
+                                                name="password" value="{{ old('password') }}" required>
                                             <label class="focus-label">Enter Password</label><span
                                                 class="fa fa-eye-slash toggle-password pt-4"></span>
                                             @error('password')
-                                            <div class="text-danger pt-2">
-                                                {{$message}}
-                                            </div>
+                                                <div class="text-danger pt-2">
+                                                    {{ $message }}
+                                                </div>
                                             @enderror
                                         </div>
 
                                         <!-- Confirm Password -->
                                         <div class="form-group form-focus">
                                             <input type="password" id="password_confirmation"
-                                                   class="form-control floating pass-input"
-                                                   name="password_confirmation" required>
+                                                class="form-control floating pass-input" name="password_confirmation"
+                                                required>
                                             <label class="focus-label">Confirm Password</label><span
                                                 class="fa toggle-password pt-4"></span>
                                             <div class="text-danger pt-2">
                                                 @error('password')
-                                                {{$message}}
+                                                    {{ $message }}
                                                 @enderror
                                             </div>
                                         </div>
@@ -85,8 +84,8 @@
                                         <div class="form-group">
                                             <div class="row">
                                                 <div class="col-12">
-                                                    <label class="custom_check mr-2 mb-0"> I agree to the <a
-                                                            href="#" class="text-primary"> terms of service</a> and
+                                                    <label class="custom_check mr-2 mb-0"> I agree to the <a href="#"
+                                                            class="text-primary"> terms of service</a> and
                                                         <a href="#" class="text-primary">privacy policy</a>
                                                         <input type="checkbox" name="radio">
                                                         <span class="checkmark"></span>
@@ -105,16 +104,16 @@
                                         </div>
                                         <!-- Social Login -->
                                         <!-- <div class="social-login">
-                                            <a href="#"><img
-                                                    src="{{ URL::asset('/assets_admin/img/icon/google.png')}}"
-                                                    class="img-fluid" alt="Logo"></a>
-                                            <a href="#"><img
-                                                    src="{{ URL::asset('/assets_admin/img/icon/facebook.png')}}"
-                                                    class="img-fluid" alt="Logo"></a>
-                                            <a href="#"><img
-                                                    src="{{ URL::asset('/assets_admin/img/icon/twitter.png')}}"
-                                                    class="img-fluid" alt="Logo"></a>
-                                        </div> -->
+                                                <a href="#"><img
+                                                        src="{{ URL::asset('/assets_admin/img/icon/google.png') }}"
+                                                        class="img-fluid" alt="Logo"></a>
+                                                <a href="#"><img
+                                                        src="{{ URL::asset('/assets_admin/img/icon/facebook.png') }}"
+                                                        class="img-fluid" alt="Logo"></a>
+                                                <a href="#"><img
+                                                        src="{{ URL::asset('/assets_admin/img/icon/twitter.png') }}"
+                                                        class="img-fluid" alt="Logo"></a>
+                                            </div> -->
                                         <!-- /Social Login -->
                                     </form>
                                     <!-- /Register Form -->
