e491f1df6ce4e59aed18fcf1ab2d91ce4f7de69c
mostafatal3t <mm3677369@gmail.com>
2025-02-14 18:58:06 +0200

    auth translation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,14 +13,10 @@
     </section>
     <!-- /Home Banner -->
     <section class="register">
-        <!-- Page Content -->
         <div class="content" style="min-height:205px;">
             <div class="container-fluid">
-
                 <div class="row">
                     <div class="col-md-8 offset-md-2">
-
-                        <!-- Register Content -->
                         <div class="account-content">
                             <div class="row align-items-center justify-content-center">
                                 <div class="col-md-7 col-lg-6 login-left">
@@ -28,8 +24,8 @@
                                         alt="Doccure Register">
                                 </div>
                                 <div class="col-md-12 col-lg-6 login-right">
-                                    <div class="login-header">
-                                        <h3>Patient Register</h3>
+                                    <div dir="{{ app()->getLocale() === 'ar' ? 'rtl' : 'ltr' }}" class="login-header">
+                                        <h3>{{ __('web.patient_register') }}</h3>
                                     </div>
 
                                     <!-- Register Form -->
@@ -38,28 +34,30 @@
                                         <div class="form-group form-focus">
                                             <input type="text" class="form-control floating" id="name_en"
                                                 name="name_en" value="{{ old('name_en') }}" required>
-                                            <label class="focus-label">Name</label>
+                                            <label class="focus-label">{{ __('web.name') }}</label>
                                             @error('name_en')
                                                 <div class="text-danger pt-2">
                                                     {{ $message }}
                                                 </div>
                                             @enderror
                                         </div>
+
                                         <div class="form-group form-focus">
                                             <input type="email" id="email_address" class="form-control floating"
-                                                name="email" required>
-                                            <label class="focus-label"> Email</label>
+                                                name="email" value="{{ old('email') }}" required>
+                                            <label class="focus-label">{{ __('web.email') }}</label>
                                             @error('email')
                                                 <div class="text-danger pt-2">
                                                     {{ $message }}
                                                 </div>
                                             @enderror
                                         </div>
+
                                         <div class="form-group form-focus">
                                             <input type="password" id="password" class="form-control floating pass-input"
-                                                name="password" value="{{ old('password') }}" required>
-                                            <label class="focus-label">Enter Password</label><span
-                                                class="fa fa-eye-slash toggle-password pt-4"></span>
+                                                name="password" required>
+                                            <label class="focus-label">{{ __('web.enter_password') }}</label>
+                                            <span class="fa fa-eye-slash toggle-password pt-4"></span>
                                             @error('password')
                                                 <div class="text-danger pt-2">
                                                     {{ $message }}
@@ -67,69 +65,57 @@
                                             @enderror
                                         </div>
 
-                                        <!-- Confirm Password -->
                                         <div class="form-group form-focus">
                                             <input type="password" id="password_confirmation"
                                                 class="form-control floating pass-input" name="password_confirmation"
                                                 required>
-                                            <label class="focus-label">Confirm Password</label><span
-                                                class="fa toggle-password pt-4"></span>
-                                            <div class="text-danger pt-2">
-                                                @error('password')
-                                                    {{ $message }}
-                                                @enderror
-                                            </div>
+                                            <label class="focus-label">{{ __('web.confirm_password') }}</label>
+                                            <span class="fa fa-eye-slash toggle-password pt-4"></span>
                                         </div>
 
                                         <div class="form-group">
                                             <div class="row">
-                                                <div class="col-12">
-                                                    <label class="custom_check mr-2 mb-0"> I agree to the <a href="#"
-                                                            class="text-primary"> terms of service</a> and
-                                                        <a href="#" class="text-primary">privacy policy</a>
-                                                        <input type="checkbox" name="radio">
+                                                <div  class="col-12">
+                                                    <label class="custom_check mr-2 mb-0">
+                                                        {{ __('web.terms_agree') }}
+                                                        <a href="#" class="text-primary">
+                                                            {{ __('web.terms_of_service') }}</a>
+                                                        {{ __('web.and') }}
+                                                        <a href="#"
+                                                            class="text-primary">{{ __('web.privacy_policy') }}</a>
+                                                        <input type="checkbox" name="terms" required>
                                                         <span class="checkmark"></span>
                                                     </label>
+                                                    @error('terms')
+                                                        <div class="text-danger pt-2">
+                                                            {{ $message }}
+                                                        </div>
+                                                    @enderror
                                                 </div>
                                             </div>
                                         </div>
+
                                         <div class="d-grid">
-                                            <button class="btn btn-primary" type="submit">Register</button>
+                                            <button class="btn btn-primary"
+                                                type="submit">{{ __('web.register') }}</button>
                                         </div>
-                                        <div class="dont-have">Already have an account? <a
-                                                href="{{ route('login') }}">Login</a></div>
+
+                                        <div dir="{{ app()->getLocale() === 'ar' ? 'rtl' : 'ltr' }}" class="dont-have">
+                                            {{ __('web.already_have_account') }}
+                                            <a href="{{ route('login') }}">{{ __('web.login') }}</a>
+                                        </div>
+
                                         <div class="login-or">
                                             <span class="or-line"></span>
-                                            <!-- <p class="span-or">or login with </p> -->
                                         </div>
-                                        <!-- Social Login -->
-                                        <!-- <div class="social-login">
-                                                <a href="#"><img
-                                                        src="{{ URL::asset('/assets_admin/img/icon/google.png') }}"
-                                                        class="img-fluid" alt="Logo"></a>
-                                                <a href="#"><img
-                                                        src="{{ URL::asset('/assets_admin/img/icon/facebook.png') }}"
-                                                        class="img-fluid" alt="Logo"></a>
-                                                <a href="#"><img
-                                                        src="{{ URL::asset('/assets_admin/img/icon/twitter.png') }}"
-                                                        class="img-fluid" alt="Logo"></a>
-                                            </div> -->
-                                        <!-- /Social Login -->
                                     </form>
-                                    <!-- /Register Form -->
-
                                 </div>
                             </div>
                         </div>
-                        <!-- /Register Content -->
-
                     </div>
                 </div>
-
             </div>
-
         </div>
-        <!-- /Page Content -->
     </section>
     <!-- /Page Content -->
 
