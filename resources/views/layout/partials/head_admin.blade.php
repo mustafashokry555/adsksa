2a6dae01d11e3cba97a8bc5f7fd917ab7cfad940
Mustafa Shokry <63380860+mustafashokry555@users.noreply.github.com>
2025-09-11 14:27:56 +0300

    favicon

--- resources/views/layout/partials/head_admin.blade.php
+++ resources/views/layout/partials/head_admin.blade.php
@@ -2,7 +2,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
         <title>@yield('title')</title>
         <!-- Favicon -->
-        <link rel="shortcut icon" href="{{asset('assets_admin/img/favicon.png')}}">
+        <link rel="shortcut icon" href="{{ asset('images/' . $setting->favicon) }}">
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="{{asset('assets_admin/libs/bootstrap/css/bootstrap.min.css')}}">
         <!-- Feathericon CSS -->
